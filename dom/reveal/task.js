4f1a7aa44dee2394d01a428d5361ccfae798ff3c
Nikita Kozyrev <nikk7100@mail.ru>
2024-05-17 19:44:43 +0300

    second commit reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,15 @@
-const reveal = document.querySelector('.reveal');
+const reveals = document.querySelectorAll('.reveal');
 
 function isInViewport(){
-    const { top, bottom } = reveal.getBoundingClientRect();
-    
-    if(top < window.innerHeight && bottom >= 0) {
-        reveal.classList.add('reveal_active');
-    } else {
-        reveal.classList.remove('reveal_active');
-    }
+    reveals.forEach((reveal) => {
+        const { top, bottom } = reveal.getBoundingClientRect();
+
+        if(top < window.innerHeight && bottom >= 0) {
+            reveal.classList.add('reveal_active');
+        } else {
+            reveal.classList.remove('reveal_active');
+        }
+    })
 };
 
 window.addEventListener('scroll', isInViewport);
