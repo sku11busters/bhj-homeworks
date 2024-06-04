0344bd1ec6467d40b8e321b41366ce2cb1a289be
Nikita Kozyrev <nikk7100@mail.ru>
2024-06-04 21:08:32 +0300

    first commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,18 @@
+const progress = document.getElementById("progress");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+  xhr.onload = () => {
+    console.log(xhr.response);
+  };
+
+  xhr.upload.addEventListener("progress", (event) => {
+    console.log(event);
+    progress.value = event.loaded / event.total;
+  });
+  xhr.send(formData);
+});
